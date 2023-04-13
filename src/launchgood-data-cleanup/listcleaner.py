47380bc2f962f27c8d379b21adabbbc71e1cf659
AhmetFahad <ahmadfahad6394@gmail.com>
2023-04-13 15:29:34 +0300

    Fix path and gitignore

--- src/launchgood-data-cleanup/listcleaner.py
+++ src/launchgood-data-cleanup/listcleaner.py
@@ -4,21 +4,23 @@ import os
 from cleanfunc import cleandf
 from checkdir import checkdir
 
-directory = Path(__file__).resolve().parent
+directory = str(Path(__file__).resolve().parent)
 all_files = os.listdir(directory)    
 csv_files = list(filter(lambda f: f.endswith('.csv'), all_files))
 dateofcreation = '07/11/2021'
 
 checkdir(directory)
 
-print("1")
+print(directory)
+print(all_files)
+print(csv_files)
 
 for i in csv_files:
-    vip, normal = cleandf(pd.read_csv(i),dateofcreation)
+    vip, normal = cleandf(pd.read_csv(directory+'/'+i),dateofcreation)
     print("1")
  #   vip = vip.drop(['index'], axis = 1).reset_index().drop(['index'], axis = 1)
  #   normal = normal.drop(['index'], axis = 1).reset_index().drop(['index'], axis = 1)
     if len(vip) > 0:
-        vip.to_csv(Path(__file__).resolve().parent/'vip/vip_'+i,index=False)
+        vip.to_csv(directory+'/vip/vip_'+i,index=False)
     if len(normal) > 0:
-        normal.to_csv(Path(__file__).resolve().parent/'normal/normal_'+i,index=False)
+        normal.to_csv(directory+'/normal/normal_'+i,index=False)
