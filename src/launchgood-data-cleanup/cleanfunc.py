65bf9b10910c2310c46574e6e076eacda96eaef0
AhmetFahad <ahmadfahad6394@gmail.com>
2023-04-13 14:10:35 +0300

    Change path function to get files

--- src/launchgood-data-cleanup/cleanfunc.py
+++ src/launchgood-data-cleanup/cleanfunc.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 def cleandf(df,tdv):
 
     df_add = pd.DataFrame(columns=df.columns)
--- src/launchgood-data-cleanup/listcleaner.py
+++ src/launchgood-data-cleanup/listcleaner.py
@@ -1,18 +1,21 @@
 import numpy as np
 import pandas as pd
-import openpyxl
+from pathlib import Path
 import os
 from cleanfunc import cleandf
 from checkdir import checkdir
 
-all_files = os.listdir(os.getcwd())    
+all_files = os.listdir(Path(__file__).resolve().parent)    
 csv_files = list(filter(lambda f: f.endswith('.csv'), all_files))
-dateofcreation = '07/11/2022'
+dateofcreation = '07/11/2021'
 
 checkdir()
 
+print("1")
+
 for i in csv_files:
     vip, normal = cleandf(pd.read_csv(i),dateofcreation)
+    print("1")
  #   vip = vip.drop(['index'], axis = 1).reset_index().drop(['index'], axis = 1)
  #   normal = normal.drop(['index'], axis = 1).reset_index().drop(['index'], axis = 1)
     if len(vip) > 0:
