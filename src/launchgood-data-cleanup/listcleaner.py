e7f28fd263738a803d869731dd0344debdbe2676
AhmetFahad <ahmadfahad6394@gmail.com>
2023-04-19 12:28:51 +0300

    Fix to_datetime function use

--- src/launchgood-data-cleanup/listcleaner.py
+++ src/launchgood-data-cleanup/listcleaner.py
@@ -1,14 +1,14 @@
 import pandas as pd
-from pathlib import Path
 import os
+from pathlib import Path
 from checkdir import checkdir
 from export import exportcsv
 
 directory = str(Path(__file__).resolve().parent)
-all_files = os.listdir(directory)    
-csv_files = list(filter(lambda f: f.endswith('.csv'), all_files))
-dateofcreation = '07/11/2021'
+all_files = os.listdir(directory)
+csv_files = list(filter(lambda f: f.endswith(".csv"), all_files))
+dateofcreation = pd.to_datetime("07/11/2021")
+
 
 checkdir(directory)
 exportcsv(directory, csv_files, dateofcreation)
-
